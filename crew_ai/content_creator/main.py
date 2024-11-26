9a5e082a8852e55fc2699f76fd282e499264ae5b
Semir Worku <semirworkuaman@gmail.com>
2024-11-26 12:57:17 +0300

    Initial Crew AI Projects

---
+++ crew_ai/content_creator/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import os
+
+from crew import (
+    CrewaiEnterpriseContentMarketingCrew,
+)
+
+
+
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {"topic": "buy used smartwatch", "company": "OkSouQ Qatar"}
+
+    result = CrewaiEnterpriseContentMarketingCrew().crew().kickoff(inputs=inputs)
+
+    return result
+
+res = run()
+print(res.pydantic.dict()['title'])
+
+
